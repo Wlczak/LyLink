b7e439636c72849ecba00dc09f4b29f63f81234d
Adam Vlček <vlcek-adam@seznam.cz>
2025-10-19 14:52:05 +0200

    new error handling logic

--- public_html/index.php
+++ public_html/index.php
@@ -18,9 +18,35 @@ $dotenv = Dotenv::createImmutable(__DIR__ . '/../config');
 $dotenv->safeLoad();
 
 $devMode = $_ENV['DEV_MODE'] === "true" ? true : false;
+
+if ($devMode) {
+} else {
+    error_reporting(E_ALL & ~E_DEPRECATED);
+}
+
 try {
-    Router::handle();
+    ob_start();
     Router::handle($devMode);
+    echo ob_get_clean();
 } catch (Exception $e) {
-    echo $e;
+    ob_clean();
+    http_response_code(500);
+    try {
+        $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates');
+        $twig = new \Twig\Environment($loader, [
+            'cache' => __DIR__ . '/../cache',
+            'debug' => $devMode
+        ]);
+        if ($devMode) {
+            echo $twig->load('error.twig')->render(['message' => $e->getMessage(), "code" => $e->getCode(), "line" => $e->getLine(), "file" => $e->getFile(), "trace" => $e->getTrace()]);
+        } else {
+            echo $twig->load('error.twig')->render(['message' => "Something has gone very wrong", "code" => $e->getCode()]);
+        }
+
+    } catch (Exception $e) {
+        echo "Something has gone very wrong";
+        if ($devMode) {
+            throw $e;
+        }
+    }
 }
